b41d15dcf27e5db960f7f9a886e420865d3a56e2
qbart <wojtowicz.bartlomiej@gmail.com>
2024-04-26 16:40:50 +0200

    feat: No public access

--- examples/cloudfront-app/main.tf
+++ examples/cloudfront-app/main.tf
@@ -7,6 +7,15 @@ resource "aws_s3_bucket" "this" {
   bucket = "apps-${random_id.example.hex}"
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 module "cloudfront_app" {
   source = "../../modules/cloudfront-app"
 
