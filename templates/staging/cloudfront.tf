5cf2bbbe61b0c004dd3efd5341b2abc3cb8498c7
Jikan7 <zontekacper@gmail.com>
2024-10-21 13:09:25 +0200

    add templates for: dev, staging, production

---
+++ templates/staging/cloudfront.tf
@@ -0,0 +1,46 @@
+resource "random_id" "cdn" {
+  byte_length = 4
+  prefix      = "tf-name-" # TODO adjust name to your project
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "apps-${random_id.cdn.hex}" 
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+module "cloudfront_app" {
+  source = "../modules/cloudfront-app"
+
+  context = {
+    namespace = "template" # TODO adjust namespace to your project name
+    stage     = "staging"
+    name      = "app"
+  }
+  app_id          = "example" # TODO adjust app_id to your project name
+  s3_bucket       = aws_s3_bucket.this.bucket
+  aliases         = []
+  certificate_arn = ""
+}
+
+module "cloudfront_deployment_policy" {
+  source = "../modules/cloudfront-deployment-policy"
+
+  s3_bucket_arn   = aws_s3_bucket.this.arn
+  cloudfront_arns = [module.cloudfront_app.arn]
+  s3_origin_arns  = [module.cloudfront_app.s3_origin_arn]
+}
+
+module "cloudfront_bucket_policy" {
+  source = "../modules/cloudfront-s3-origin-bucket-policy"
+
+  s3_bucket       = aws_s3_bucket.this.bucket
+  cloudfront_arns = [module.cloudfront_app.arn]
+}
---
+++ templates/staging/networking.tf
@@ -0,0 +1,78 @@
+locals {
+  context = {
+    namespace = "name" # TODO adjust name to your project
+    stage     = "staging"
+    name      = "networking"
+  }
+}
+
+module "vpc" {
+  source = "../modules/vpc"
+
+  name = random_id.example.hex
+  cidr = "10.0.0.0/16"
+}
+
+module "public_subnet" {
+  source = "../modules/vpc-public-subnet"
+
+  vpc_id              = module.vpc.id
+  internet_gateway_id = module.vpc.internet_gateway_id
+  config = {
+    "a" = {
+      az   = "eu-central-1a"
+      cidr = "10.0.1.0/24"
+      nat  = true
+    }
+    "b" = {
+      az   = "eu-central-1b"
+      cidr = "10.0.2.0/24"
+      nat  = false
+    }
+  }
+}
+
+module "private_subnet" {
+  source = "../modules/vpc-private-subnet"
+
+  context = local.context
+  vpc_id  = module.vpc.id
+
+  config = {
+    "a" = {
+      az   = "eu-central-1a"
+      cidr = "10.0.51.0/24"
+    }
+  }
+
+  nat_gateway_routing = {
+    "a" = module.public_subnet.nat_gateways["eu-central-1a"]
+  }
+}
+
+module "database_subnet" {
+  source = "../modules/vpc-private-subnet"
+
+  context = local.context
+  vpc_id  = module.vpc.id
+
+  config = {
+    "db-a" = {
+      az   = "eu-central-1a"
+      cidr = "10.0.101.0/24"
+    }
+  }
+
+  database_subnet = true
+}
+
+module "lb" {
+  source = "../modules/lb/alb"
+
+  name        = random_id.example.hex
+  vpc_id      = module.vpc.id
+  subnet_ids  = module.public_subnet.ids
+  force_https = false
+
+  context = local.context
+}
