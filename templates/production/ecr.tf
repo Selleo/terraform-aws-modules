ccad9c09126d994d3fb656c72b82b7892ed40996
Jikan7 <zontekacper@gmail.com>
2024-12-11 07:22:40 +0100

    add new files for dev environment

--- templates/production/ecr.tf
+++ templates/production/ecr.tf
@@ -1,12 +1,3 @@
-module "info" {
-  source  = "Selleo/context/null"
-  version = "0.3.0"
-
-  namespace = "name" # TODO adjust name to your project
-  stage     = "production"
-  name      = "api"
-}
-
 module "ecr" {
   source = "../modules/ecr-repository"
 
